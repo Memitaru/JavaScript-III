9e502ef28d1a72a581ab42b08cf4ffa412e473a9
Memitaru <amandatwah@gmail.com>
2019-01-22 00:34:05 -0800

    Completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - When this is used in the global scope then 'this' will be the window.
+* 2. Implicit binding - When calling a function inside of an object 'this' will become the name of the object.
+* 3. New Binding - When using new to create an object with a constructor function 'this' will be the new object that is created.
+* 4. Explicit binding - If you are using 'this' but use .call or .apply then 'this' will change from default to the object you've specified. 
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,40 @@
 
 // code example for Window Binding
 
+function thisWindow(){
+    console.log(this);
+}
+thisWindow();
+
 // Principle 2
 
-// code example for Implicit Binding
+const myObj = {
+    name: "Runty",
+    age: "9",
+    tail: "short",
+    testFunction: function(type){
+        console.log(`My name is ${this.name} and I am a ${this.age} year old ${type}.`)
+    }
+}
+
+myObj.testFunction("cat");
 
 // Principle 3
 
-// code example for New Binding
+function Cats(name){
+    this.name = name,
+    this.speak = function(){
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+const mowm = new Cats("Mowm");
+mowm.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+
+const jazzy = new Cats("Jazzy");
+
+jazzy.speak.call(mowm);
